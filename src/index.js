7c56d22e8e474b62e6fa161ed76169decc8587a6
Martin Clark <webdeveloperplug@gmail.com>
2023-02-21 17:48:34 -0600

    Lets see if this works or not.

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (message) => {
     let chat_id = message.from.id;
     bot.sendMessage(chat_id, 'Peace, I am I/O!');
+    console.log(message);
 });
