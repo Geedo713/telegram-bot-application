2721a437453f4e5363a662ba60d83b45bd285509
Martin Clark <webdeveloperplug@gmail.com>
2023-02-22 17:35:06 -0600

    This dam workflow has been disabled hopefully this change makes it work.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
 const token = process.env.TOKEN;
-const dummy = process.env.DUMB;
 //const bot = new TelegramBot(dummy, {polling: true});
 const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (message) => {
