20bc4ca27df531257857af2387232e100cdc697c
Martin Clark <webdeveloperplug@gmail.com>
2023-02-21 21:32:49 -0600

    Some late night updates before going to bed.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
 const token = process.env.TOKEN;
+const dummy = process.env.DUMB;
+//const bot = new TelegramBot(dummy, {polling: true});
 const bot = new TelegramBot(token, {polling: true});
-
 bot.on('message', (message) => {
     let chat_id = message.from.id;
-    bot.sendMessage(chat_id, 'Peace, I am I/O!');
-    console.log(message);
+    let alias = message.from.first_name;
+    bot.sendMessage(chat_id, 'Hello ' + alias + ', how are you doing today?');
+    //console.log(message);
+});
+
+bot.onReplyToMessage('message', (message) => {
+    let chat_id = message.from.id;
+    let alias = message.from.username;
 });
